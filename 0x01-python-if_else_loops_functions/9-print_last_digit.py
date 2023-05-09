612f1bac2a38b3d0712565f41cb3cdd747a13f12
salmalah <s.salmalahmidi@gmail.com>
2023-05-09 11:30:13 -0700

    njreb

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    if number < 0:
+        l_digit = (number % -10)* -1
+    else:
+        l_digit = number % 10
+    print("{}".format(l_digit), end='')
+    return l_digit
