cabd41c57f6ff4a5b19f7093c35cbf55df94495c
salmalah <s.salmalahmidi@gmail.com>
2023-05-11 10:22:41 -0700

    fix text error

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,4 +9,4 @@ if __name__ == "__main__":
     else:
         print("{} arguments:".format(ln))
     for i in range(ln):
-        print("{} : {}".format(i + 1, sys.argv[i + 1]))
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
