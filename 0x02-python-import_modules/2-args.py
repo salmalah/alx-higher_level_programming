b05014ce77606e27801bccc854f3031df30c672e
salmalah <s.salmalahmidi@gmail.com>
2023-05-11 10:21:22 -0700

    fix text error

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,8 +5,8 @@ if __name__ == "__main__":
     if ln == 0:
         print("0 arguments.")
     elif ln == 1:
-        print("1 arguments:")
+        print("1 argument:")
     else:
-        print("{} argument:".format(ln))
+        print("{} arguments:".format(ln))
     for i in range(ln):
         print("{} : {}".format(i + 1, sys.argv[i + 1]))
