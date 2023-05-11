8b87d924af586d4999068ebf568b49521a8b3e8f
salmalah <s.salmalahmidi@gmail.com>
2023-05-11 08:39:17 -0700

    fix error code should not be executed when imported - by using __import_

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-from add_0 import add
-a = 1
-b = 2
-addition_result = add(a, b)
-print("{} + {} = {}".format(a, b, addition_result))
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    addition_result = add(a, b)
+    print("{} + {} = {}".format(a, b, addition_result))
