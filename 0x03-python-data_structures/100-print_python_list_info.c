c01671fa129acb5879cf03993167fa65c6e9a620
salmalah <s.salmalahmidi@gmail.com>
2023-05-15 16:49:01 -0700

    test

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,12 @@
+#include <Python.h>
+#include <stdio.h>
+/**
+ * print_python_list_info - A function that prints some basic info about Python lists
+ *@p: object types for Python
+ */
+void print_python_list_info(PyObject *p)
+{
+	Py_ssize_t list_size;
+	printf("[*] Size of the Python List = %d\n", list_size); 
+}
+
