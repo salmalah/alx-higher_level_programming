38ca721f25d4049f361d9465cf26fdd26d296ce1
salmalah <s.salmalahmidi@gmail.com>
2023-05-17 12:48:13 -0700

    test

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    if a_dictionary:
+        found_v = [k for k, v in a_dictionary.items() if v == value]
+        for v_d in found_v:
+            del a_dictionary[v_d]
+        return a_dictionary
