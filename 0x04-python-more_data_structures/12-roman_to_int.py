e98e4bf794e8e92cccc870ca9d4dd58b2daa1985
salmalah <s.salmalahmidi@gmail.com>
2023-05-17 13:22:58 -0700

    test

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,7 +4,8 @@ def roman_to_int(roman_string):
         return 0
 
     diction = dict(I=1, V=5, X=10, L=50, C=100, D=500, M=1000)
-    n, prev_value = 0
+    n = 0
+    prev_value = 0
     for char in reversed(roman_string):
         value = diction.get(char, 0)
         if value == 0:
