f031ae0065b442614074368c9da0b167e3261160
salmalah <s.salmalahmidi@gmail.com>
2023-05-17 14:16:37 -0700

    test

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -39,7 +39,7 @@ void print_python_bytes(PyObject *p)
 	PyBytesObject *byt= (PyBytesObject *)p;
 
 	printf("[.] bytes object info\n");
-	if (strcmp(p->ob_type->tp_name, "bytes") != 0)
+	if (strcmp((*p).ob_type->tp_name, "bytes") != 0)
 	{
 		printf("  [ERROR] Invalid Bytes Object\n");
 		return;
