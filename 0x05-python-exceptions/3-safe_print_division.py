2b948bc7ff53c10242dfd0b61d2b5b51deff06f4
salmalah <s.salmalahmidi@gmail.com>
2023-05-29 11:13:34 -0700

    fix error pycodestyle over-indented

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,10 +2,9 @@
 def safe_print_division(a, b):
     div = 0
     try:
-         div = a / b
+        div = a / b
     except ZeroDivisionError:
         div = None
     finally:
         print("Inside result: {}".format(div))
         return div
-
