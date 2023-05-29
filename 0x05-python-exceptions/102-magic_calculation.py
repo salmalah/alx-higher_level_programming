b39ba5618b00daf02bab1214aa0a4ddeb1a15ec4
salmalah <s.salmalahmidi@gmail.com>
2023-05-29 12:49:37 -0700

    9. ByteCode -> Python #4

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    po = 0
+    for ind in range(1, 4):
+        try:
+            if ind > a:
+                raise Exception("Too far")
+            po += pow(a, b) / ind
+        except:
+            po = b + a
+            break
+        return po
