513d7d5e7230d89a38d8d37890290391f0642d9e
salmalah <s.salmalahmidi@gmail.com>
2023-05-29 12:55:18 -0700

    test

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
     po = 0
-    for ind in range(1, 4):
+    for ind in range(1, 3):
         try:
             if ind > a:
                 raise Exception("Too far")
