1ffe59c13df4f4b323861ba2110d03263ea846f3
salmalah <s.salmalahmidi@gmail.com>
2023-05-29 10:57:36 -0700

    fix pycodestyle error

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    count = 0;
+    count = 0
     try:
         for index in range(x):
             if isinstance(my_list[index], int):
@@ -10,4 +10,3 @@ def safe_print_list_integers(my_list=[], x=0):
         pass
     print()
     return count
-
