4b99ce736ddac94a54bc8c2d422da8bb549c2f40
salmalah <s.salmalahmidi@gmail.com>
2023-05-29 12:28:23 -0700

    7. Safe integer print with error message

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,6 +5,6 @@ def safe_print_integer_err(value):
         if isinstance(value, int):
             print("{:d}".format(value))
             return True
-    except Exception as m:
+    except  except (ValueError, TypeError) as m:
         print("Exception: {}".format(m), file=sys.stderr)
         return False
