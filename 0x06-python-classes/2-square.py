3ca2f3202379c995ae99bac806e0742357f4b838
salmalah <s.salmalahmidi@gmail.com>
2023-05-30 11:27:47 -0700

    fix errors pycodestyle

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -14,7 +14,7 @@ Methodes:
 
 class Square:
     """Defines a square"""
-    def __init__(self, size = 0):
+    def __init__(self, size=0):
         """
         Initializes a new instance of the Square class.
 
