b62c7620606d19b8d15a1fdb9c4798eaa8766b07
salmalah <s.salmalahmidi@gmail.com>
2023-06-06 13:36:54 -0700

    test

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
+"""
+A class LockedClass with no object
+Attribute:
+    No
+"""
 class LockedClass:
+    """creating new instance attributes
+    except if the new instance attribute is called first_name
+    """
     __slots__ = ['first_name']
