ed2e1fc233d107b0ba208ed41b9bb03a138606a5
salmalah <s.salmalahmidi@gmail.com>
2023-06-08 08:47:17 -0700

    test

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+"""
+Function that multiplies 2 matrices
+"""
 def matrix_mul(m_a, m_b):
+    """
+    Multiply two matrices.
+    """
     # Validate m_a and m_b
     if not isinstance(m_a, list) or not isinstance(m_b, list):
         raise TypeError("m_a must be a list or m_b must be a list")
