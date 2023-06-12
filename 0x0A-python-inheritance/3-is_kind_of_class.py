b5bfe5ecd61989041718b449b513d17caf4160ce
salmalah <s.salmalahmidi@gmail.com>
2023-06-12 13:35:19 -0700

    3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Function check if the object is: 
+instance /an instance of a class
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    Initialise Functoion 
+    Return:
+        True if the object is an instance
+        False if instannce of class
+    """
+
+    return(isinstance(obj, a_class))
