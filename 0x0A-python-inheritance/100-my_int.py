f6c310adde20553ddc4deeee0a18bad04cbaf876
salmalah <s.salmalahmidi@gmail.com>
2023-06-12 14:53:12 -0700

    test

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -7,32 +7,19 @@ module class Myint
 class MyInt(int):
     """
     A class representing a rebel integer
-    Attributes:
-        No additional attributes.
-
-    Methods:
-        __eq__(self, other): Overrides the == operator
-        __ne__(self, other): Overrides the != operator
     """
-
     def __eq__(self, other):
         """
         Overrides the == operator
-
-        Args:
-            other: The value to compare
         Return:
             True if the values are not equal or flse
         """
-        return super().__ne__(other)
+        return (super().__ne__(other))
 
     def __ne__(self, other):
         """
         Overrides the != operator
-        Arg:
-            other: The value to compare with
-
         Return
             True if the values are equal,or False
         """
-        return super().__eq__(other)
+        return (super().__eq__(other))
