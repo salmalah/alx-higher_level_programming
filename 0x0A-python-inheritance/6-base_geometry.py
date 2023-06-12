8d5ad9c161db218480bd3fdd2ec65bb11b4046d0
salmalah <s.salmalahmidi@gmail.com>
2023-06-12 13:44:33 -0700

    6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+module an empty class
+"""
+
+
+class BaseGeometry:
+    """
+    define the empty class
+    method:
+       Public instance method: def area(self)
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
