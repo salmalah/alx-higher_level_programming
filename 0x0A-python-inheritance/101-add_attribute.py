b499f71ea1feb1e64a34ed3a63272b4eb9cc8b2e
salmalah <s.salmalahmidi@gmail.com>
2023-06-12 14:55:04 -0700

    fte

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Function adds attributes to an object
+"""
+
+
+def add_attribute(obj, name, value):
+    """
+    Intialise functions 
+    Adds attributes
+    """
+    if '__dict__' not in dir(obj):
+        raise TypeError("can't add new attribute")
+    else:
+        setattr(obj, name, value)
