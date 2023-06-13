e2e5e145e9d713fb5f827d75d66785244688dac2
salmalah <s.salmalahmidi@gmail.com>
2023-06-13 13:04:44 -0700

    7

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+a script that adds all arguments to a Python list
+"""
+import sys
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+fs = "add_item.json"
+if path.isfile(fs):
+    ls = load_from_json_file(filename)
+else:
+    ls = []
+ls.extend(sys.argv[1:])
+save_to_json_file(ls, fs)
