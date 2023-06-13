8c81e01ef897d0f75ad799062780030863aadbeb
salmalah <s.salmalahmidi@gmail.com>
2023-06-13 12:35:43 -0700

    1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Defines function that writes a string to a text file (UTF8)
+"""
+
+def write_file(filename="", text=""):
+    """
+    initialise function
+    Args:
+        filename: name of the file to write
+        text: string to write to the file
+    Return:
+        the number of characters written
+    """
+    with open(filename, mode='w', encoding='utf-8') as f:
+        charac = f.write(text)
+    return chara
