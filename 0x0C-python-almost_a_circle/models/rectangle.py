d9d5a8b07dc6da1085e0c61e3659bcb5426f85f6
salmalah <s.salmalahmidi@gmail.com>
2023-06-19 10:21:02 -0700

    2.test

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -19,3 +19,61 @@ class Rectangle(Base):
         self.y = y
         super().__init__(id)
 
+    @property
+    def height(self):
+        """
+        Getter method for the height attribute
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """
+        Setter method for the height attribute
+        """
+        self.__height = value
+
+    @property
+    def width(self):
+        """
+        Getter method for the width attribute
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """
+        Setter method for the width attribute
+        """
+        self.__width = value
+
+    @property
+    def y(self):
+        """
+        Getter method for the y attribute
+        """
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """
+        Setter method for the y attribute
+        """
+        self.__y = value
+
+    @property
+    def x(self):
+        """
+        Getter method for the x attribute
+        """
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """
+        Setter method for the x attribute
+        """
+        self.__x = value
+
+
+
