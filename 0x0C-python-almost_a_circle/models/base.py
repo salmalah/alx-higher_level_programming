5603d1b1b8b414b2763b092c70d0e66b1c36528e
salmalah <s.salmalahmidi@gmail.com>
2023-06-19 09:58:59 -0700

    1.test

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+define a class Base"
+"""
+
+class Base:
+    """
+    Base class
+    declare the private attribute
+    """
+    __nb_objects = 0
+    """
+    Initializes a new instance
+    """
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
