66a21f282626b8cfbdf04434e299d670f8683656
salmalah <s.salmalahmidi@gmail.com>
2023-06-19 11:16:44 -0700

    14.test

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -26,7 +26,7 @@ class Base:
         """
         returns the JSON string representation of list_dictionaries
         """
-        if len(list_dictionaries) == 0 or list_dictionaries is None:
+        if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
