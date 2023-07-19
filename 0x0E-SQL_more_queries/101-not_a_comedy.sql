bb057b05468964ecf97b0ee90045788cf90d0c32
salmalah <s.salmalahmidi@gmail.com>
2023-07-19 12:58:21 -0700

    fix errors

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,7 +1,10 @@
--- a script that lists all shows without the genre Comedy in the database hbtn_0d_tvshows
-SELECT title
-FROM tv_shows
-WHERE id NOT IN (
-	  SELECT show_id FROM tv_show_genres WHERE genre_id = (
-		    SELECT id FROM tv_genres WHERE name = 'Comedy'))
-	ORDER BY title;
+-- a script that lists all shows without the genre Comedy in the database hbtn_0d_tvshows 
+SELECT DISTINCT `title`
+ FROM `tv_shows` AS tv LEFT JOIN `tv_show_genres` AS ts
+       ON ts.`show_id` = tv.`id` LEFT JOIN `tv_genres` AS tg
+       ON tg.`id` = ts.`genre_id` WHERE tv.`title` NOT IN
+             (SELECT `title`
+                FROM `tv_shows` AS tv INNER JOIN `tv_show_genres` AS ts
+		     ON ts.`show_id` = tv.`id` INNER JOIN `tv_genres` AS tg
+		     ON tg.`id` = ts.`genre_id` WHERE tg.`name` = "Comedy")
+ORDER BY `title`;
