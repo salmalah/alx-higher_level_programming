cc54af7381d945da71766ba41e08724ebba41b08
salmalah <s.salmalahmidi@gmail.com>
2023-08-15 11:39:56 -0700

    fix error

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -2,7 +2,7 @@
 exports.nbOccurences = function (list, searchElement) {
   let count = 0;
   for (const elem of list) {
-    if (eleme === searchElement) {
+    if (elem === searchElement) {
       count++;
     }
   }
