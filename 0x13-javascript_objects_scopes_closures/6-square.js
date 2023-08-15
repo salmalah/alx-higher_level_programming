ee7785f0a5cd6f197a98cc5f0af2d5e914bb5bb6
salmalah <s.salmalahmidi@gmail.com>
2023-08-15 11:35:25 -0700

    6. Square #1

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const PrSquare = require('./5-square');
+class Square extends PrSquare {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+}
+module.exports = Square;
