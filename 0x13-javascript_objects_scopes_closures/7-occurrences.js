b103b6cc95b14e2f7a93bf037b27e2c38db6af9f
salmalah <s.salmalahmidi@gmail.com>
2023-08-15 11:38:04 -0700

    7. Occurrences

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+exports.nbOccurences = function (list, searchElement) {
+  let count = 0;
+  for (const elem of list) {
+    if (eleme === searchElement) {
+      count++;
+    }
+  }
+  return count;
+};
