e449a4033ba4fa17c4800a3358424f7c49eb1c7e
salmalah <s.salmalahmidi@gmail.com>
2023-08-20 16:17:55 -0700

    tasks 11 && 12

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+define a script that changes the name of a State object
+from the database hbtn_0e_6_usa
+"""
+import sys as f
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import State
+
+if __name__ == "__main__":
+    u_name = f.argv[1]
+    pass_d = f.argv[2]
+    db_name = f.argv[3]
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(u_name, pass_d, db_name),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    ss = Session()
+    st = ss.query(State).filter_by(id=2).first()
+    st.name = "New Mexico"
+    ss.commit()
