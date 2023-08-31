28ab3e6fe88fd9f822d80ea75f8e633fe5eb452d
salmalah <122815714+salmalah@users.noreply.github.com>
2023-08-31 21:34:59 +0100

    Update 102-catch_me.sh

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # a Bash script that makes a request to 0.0.0.0:5000/catch_me
-curl -s -X POST -H "User-Agent: You got me!" 0.0.0.0:5000/catch_me
+curl -sw -X POST -H "User-Agent: You got me!" 0.0.0.0:5000/catch_me
