dd13c7bf609ae63fb6fd92e33904c575017c4d7d
salmalah <s.salmalahmidi@gmail.com>
2023-09-01 17:17:01 -0700

    try to fix error

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 """
-a Python script that takes in a URL and an email, sends a POST request to the passed URL
+a Python script that takes in a URL and an email, sends a POST request
 """
 import urllib.parse as up
-import urllib.request as ur 
+import urllib.request as ur
 import sys as s
 
 
 if __name__ == "__main__":
     u = s.argv[1]
     e = s.argv[2]
-    data = up.urlencode({'email': e}).encode('ascii')
-    req = ur.Request(u, data)
+    d = up.urlencode({'email': e}).encode('ascii')
+    req = ur.Request(u, d)
     with ur.urlopen(req) as re:
         content = re.read().decode('utf-8')
         print(content)
