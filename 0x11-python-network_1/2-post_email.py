5d51c82071957bc8689ba198f08104d8d02153ee
salmalah <s.salmalahmidi@gmail.com>
2023-09-01 17:06:12 -0700

    fix errors

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -2,14 +2,15 @@
 """
 a Python script that takes in a URL and an email, sends a POST request to the passed URL
 """
+import urllib.parse as up
 import urllib.request as ur 
 import sys as s
 
 if __name__ == "__main__":
     u = s.argv[1]
     e = s.argv[2]
-    data = urllib.parse.urlencode({'email': e}).encode('utf-8')
-    req = ur.Request(url, data=data, method='POST')
+    data = up.urlencode({'email': e}).encode('utf-8')
+    req = ur.Request(u, data=data, method='POST')
     with ur.urlopen(req) as re:
         content = re.read().decode('utf-8')
     print(content)
