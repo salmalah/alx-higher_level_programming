2686d317ef723d8e8c483b34e32c0dd64056b777
salmalah <s.salmalahmidi@gmail.com>
2023-09-01 17:12:15 -0700

    test code

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -10,8 +10,8 @@ import sys as s
 if __name__ == "__main__":
     u = s.argv[1]
     e = s.argv[2]
-    data = up.urlencode({'email': e}).encode('utf-8')
-    req = ur.Request(u, data=data, method='POST')
+    data = up.urlencode({'email': e}).encode('ascii')
+    req = ur.Request(u, data)
     with ur.urlopen(req) as re:
         content = re.read().decode('utf-8')
         print(content)
