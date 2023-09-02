fc05c7fa70420bc51f922cfca1295ba11e3a1b61
salmalah <s.salmalahmidi@gmail.com>
2023-09-01 17:58:41 -0700

    7. Error code #1

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+a script that takes in a URL sends a request to the URL and displays the body
+"""
+import sys
+import requests as r
+
+
+if __name__ == "__main__":
+    u = sys.argv[1]
+    try:
+        re = r.get(u)
+        content = re.text
+        print(content)
+    except r.exceptions.HTTPError as m:
+        print(f"Error code: {m.response.status_code}")
