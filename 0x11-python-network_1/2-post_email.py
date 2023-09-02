ebd75a40d06499d8000d41f77190c01cfb5a03d4
salmalah <s.salmalahmidi@gmail.com>
2023-09-01 17:09:16 -0700

    test code

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -6,6 +6,7 @@ import urllib.parse as up
 import urllib.request as ur 
 import sys as s
 
+
 if __name__ == "__main__":
     u = s.argv[1]
     e = s.argv[2]
@@ -13,4 +14,4 @@ if __name__ == "__main__":
     req = ur.Request(u, data=data, method='POST')
     with ur.urlopen(req) as re:
         content = re.read().decode('utf-8')
-    print(content)
+        print(content)
