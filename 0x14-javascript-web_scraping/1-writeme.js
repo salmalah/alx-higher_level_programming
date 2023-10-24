6fea122e6d5efb37e8007808b544a537007a4cf6
salmalah <s.salmalahmidi@gmail.com>
2023-10-24 11:51:57 -0700

    1. Write me

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,9 +2,9 @@
 const fs = require('fs');
 
 const filePath = process.argv[2];
-const string_content = process.argv[3];
+const stringContent = process.argv[3];
 
-fs.writeFile(filePath, string_content, 'utf-8', (err) => {
+fs.writeFile(filePath, stringContent, 'utf-8', (err) => {
   if (err) {
     console.log(err);
   }
